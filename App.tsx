a17e7c08035d9d6d67c90130af1d03a0ec1224e9
red-mat <jesus.ing.rodriguez@gmail.com>
2023-09-19 16:44:50 -0300

    chore: add eslint

--- App.tsx
+++ App.tsx
@@ -1,8 +1,9 @@
 import 'react-native-gesture-handler';
-import Navigation from './src/navigation/Navigation';
-import store from './src/context/store';
 import { Provider } from 'react-redux';
+
 import { TicketProvider } from './src/context/TicketContext';
+import store from './src/context/store';
+import Navigation from './src/navigation/Navigation';
 //import CarritoContext from "./src/context/carritoContext";
 
 export default function App() {
