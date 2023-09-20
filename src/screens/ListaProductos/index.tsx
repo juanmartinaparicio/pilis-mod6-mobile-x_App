a61128b6db76857301a0fced57a2d9fcdd9066ef
red-mat <jesus.ing.rodriguez@gmail.com>
2023-09-19 22:19:40 -0300

    refactor(screens): ListaProducts

---
+++ src/screens/ListaProductos/index.tsx
@@ -0,0 +1,50 @@
+import React, { useEffect } from 'react';
+import { SafeAreaView, ScrollView, StyleSheet, Text } from 'react-native';
+import { useDispatch, useSelector } from 'react-redux';
+
+import CardProduct from './CardProduct';
+import { getProducts } from '../../context/redux/reducers/ProductReducer';
+
+import getAllProducts from '@/services/products/getAllProducts';
+import { Product } from '@/services/products/types';
+
+export default function ListaProductos() {
+  const products = useSelector((state: any) => state.product.product);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const response = await getAllProducts();
+      if (response.isError) return;
+
+      const { result: products } = response;
+      products?.map(product => dispatch(getProducts(product)));
+    };
+
+    fetchProducts();
+  }, []);
+
+  return (
+    <SafeAreaView style={{ backgroundColor: '#fff' }}>
+      <ScrollView contentContainerStyle={styles.container}>
+        <Text style={styles.title}>Restaurant Menu</Text>
+
+        {products?.map((product: Product) => (
+          <CardProduct key={product.id} item={product} />
+        ))}
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 24,
+  },
+  title: {
+    fontSize: 32,
+    fontWeight: '700',
+    color: '#1d1d1d',
+    marginBottom: 12,
+  },
+});
