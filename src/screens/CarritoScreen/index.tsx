5e822176d5447ac07f55b1bb60317d96a169b72e
red-mat <jesus.ing.rodriguez@gmail.com>
2023-09-22 20:22:19 -0300

    refactor(screens): update order button

--- src/screens/CarritoScreen/index.tsx
+++ src/screens/CarritoScreen/index.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { View } from 'react-native';
 
 import Cart from './Cart';
+import Orders from './Orders';
 
 export default function CarritoScreen() {
   return (
     <View>
       <Cart />
+      <Orders />
     </View>
   );
 }
