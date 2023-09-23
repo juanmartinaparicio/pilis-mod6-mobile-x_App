34aebae733c61d78d85d5c4e3ba40c0524850935
red-mat <jesus.ing.rodriguez@gmail.com>
2023-09-23 01:18:42 -0300

    fix(screens): resolve Orders CarritoScreen

--- src/screens/CarritoScreen/Orders/index.tsx
+++ src/screens/CarritoScreen/Orders/index.tsx
@@ -1,9 +1,9 @@
 import { Text, View } from 'react-native';
 
-import useOrder from '../context/hook';
-
 export default function Orders() {
-  const { orders, isLoading, refreshOrder, refreshOrders } = useOrder();
-
-  return <View>{isLoading && <Text>cargando...</Text>}</View>;
+  return (
+    <View>
+      <Text>orders</Text>
+    </View>
+  );
 }
