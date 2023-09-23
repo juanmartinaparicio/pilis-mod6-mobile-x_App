3d205d2da50b27e74414115558beb810e075f3f4
red-mat <jesus.ing.rodriguez@gmail.com>
2023-09-23 00:24:34 -0300

    feat(services): add refresh token orders

---
+++ src/services/orders/refreshToken.ts
@@ -0,0 +1,21 @@
+import { Order } from './types';
+
+import ENDPOINTS from '@/utils/endpoints';
+import { Response } from '@/utils/request';
+
+type Result = Response<Order>;
+async function refreshToken(orderId: string): Promise<Result> {
+  const url = `${ENDPOINTS.REFRESH_ORDER}/${orderId}`;
+  const response = await fetch(url);
+
+  const status = response.status;
+  const isError = !response.ok;
+
+  if (isError) return { status, isError };
+
+  const result = await response.json();
+
+  return { status, isError, result };
+}
+
+export default refreshToken;
